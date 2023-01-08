19cce4a716dcbac36c5182276af5e151affc80f8
PedroHenriqueSilva <pedrohsilva.ccomp@gmail.com>
2023-01-08 17:03:30 -0300

    arrays

--- script.js
+++ script.js
@@ -68,3 +68,107 @@ console.log(fullName.toUpperCase())
 
 console.log(fullName.indexOf('Silva'))
 console.log(fullName.slice(7,13))
+
+//métodos de arrays
+const list = ['a', 'b', 'c', 'd', 'e', 'f']
+
+console.log(list.length)
+console.log(list[5])
+
+list[6] = 'g'
+console.log(list)
+
+console.log(list[list.length - 2])
+
+list.push('h')
+
+console.log(list)
+
+list.shift()
+console.log(list)
+
+list.unshift('z')
+console.log(list)
+
+
+//Objetos
+
+const product = {
+    name:"Camisa",
+    price:"1,79", 
+    inStock:true, 
+    sizes:['p', 'm', 'g']
+}
+
+console.log(product.name)
+console.log(product.price)
+console.log(product.inStock)
+console.log(Object.keys(product).length)
+
+
+//estruturaas de repetição
+
+const mylist= ['11','2','3']
+let count = 0
+
+while(count < mylist.length){
+    console.log(mylist[count])
+    count++
+}
+
+const mySecondList = ['a', 'b', 'c', 'd', 'e']
+
+for(let count = 0; count<mySecondList.length;count++){
+
+    console.log("Imprimindo: " + mySecondList[count])
+}
+
+const names = ['Pedro', 'Joao', 'Ana']
+
+//foreach
+
+names.forEach(function(n){
+    console.log("Imprimindo:" +  n) 
+})
+
+const modificNames = names.map(function(n){
+    if(n ==="Pedro" ){
+        return(n = "Sr.Pedro")
+    }else{
+        return n
+    }
+
+}
+
+)
+
+console.log(modificNames)
+
+const bigNumbers = [1,2,3,4,5,10,100].filter(function(number){
+    return number >= 5
+})
+
+console.log(bigNumbers.length)
+
+const sum = [1,2,3,4,5].reduce((total, number) =>{
+    return total+number
+})
+
+console.log(sum)
+
+//function
+function nomeCompleto(nome, sobrenome){
+    return "meu nome é: " + nome + " " + sobrenome
+}
+
+console.log(nomeCompleto('Pedro', 'Silva'))  
+console.log(nomeCompleto('Joao', 'Antonio'))
+
+
+
+//arrowFunction
+
+const myArrowFunction = (a,b) => {
+    return a + b
+}
+console.log(myArrowFunction(1, 5)) 
